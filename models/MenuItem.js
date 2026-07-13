503aa4f9316b6f10b494036797037733ab8c2c45
sshreya007 <shreyakhadka007@gmail.com>
2026-07-13 14:01:05 +0545

    Working on menuitem models

---
+++ models/MenuItem.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const menuItemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    description: { type: String, default: '' },
+    restaurant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Restaurant',
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('MenuItem', menuItemSchema);
