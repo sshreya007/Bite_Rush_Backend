91e6e4bd38ef6870f374eb28945a28805a9ea0dc
sshreya007 <shreyakhadka007@gmail.com>
2026-07-17 11:08:26 +0545

    changes in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const offerRoutes = require('./routes/offerRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/restaurants', restaurantRoutes);
 app.use('/api/offers', offerRoutes);
+app.use('/api/orders', orderRoutes);
 
 // Health check
 app.get('/', (req, res) => {
